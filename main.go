7bbe58cf2d86c59966051663c11b784b17a99cd7
Aqazde <adil64489@gmail.com>
2023-12-22 13:42:42 +0600

    modifications for POST request

--- main.go
+++ main.go
@@ -1,5 +1,4 @@
 package main
-
 import (
 	"encoding/json"
 	"fmt"
@@ -17,9 +16,29 @@ func fetchData() map[string]interface{} {
 }
 
 func handleGetData(w http.ResponseWriter, r *http.Request) {
-	data := fetchData()
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	if r.Method == http.MethodGet {
+		data := fetchData()
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(data)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlePostData(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var receivedData map[string]interface{}
+		if err := json.NewDecoder(r.Body).Decode(&receivedData); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fmt.Printf("Received data: %#v\n", receivedData)
+		response := map[string]string{"message": "Data received successfully"}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
 func serveHTML(w http.ResponseWriter, r *http.Request) {
@@ -28,8 +47,9 @@ func serveHTML(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/getdata", handleGetData)
+	http.HandleFunc("/postdata", handlePostData)
 	http.HandleFunc("/", serveHTML)
 
-	fmt.Println("Server listening on port 1997")
-	log.Fatal(http.ListenAndServe(":1997", nil))
+	fmt.Println("Server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
